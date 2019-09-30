d25561d29726109282d6fc71c6290d72234cd50e
jake hartley <puffleboy101@hotmail.co.uk>
2019-09-30 14:52:12 +0100

    -commit

--- DefewayMultiplex/Form1.Designer.cs
+++ DefewayMultiplex/Form1.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 535);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(189, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(836, 562);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Import";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(171, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Dir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(171, 433);
+            this.listBox1.TabIndex = 7;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 538);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(106, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Random IP order";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1037, 586);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Defeway multiplexer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- DefewayMultiplex/Form1.cs
+++ DefewayMultiplex/Form1.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +17,270 @@ namespace DefewayMultiplex
 {
     public partial class Form1 : Form
     {
+        public string username = "admin";
+        public string password = "";
+        public List<String> ips = new List<String> { };
+        public string selectedDirectory = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public int getCamCount(string ipi)
+        {
+            string ip;
+
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                listBox1.Items.Insert(0, "Testing: " + ipi);
+            }));
+
+            if (ipi.Contains("https://"))
+            {
+                ip = ipi;
+            }
+            else
+            {
+                ip = @"http://" + ipi;
+            }
+
+            string[] ipPingArr = ip.Split(':');
+            string ipPing = ipPingArr[1];
+            ipPing = ipPing.Replace("//", "");
+            var p = new Ping();
+            PingReply reply = p.Send(ipPing, 1000);
+
+            if (reply.Status == IPStatus.Success)
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    listBox1.Items.Insert(0, "Ping success.");
+                }));
+                string data = "/cgi-bin/gw.cgi?xml=<juan%20ver=\"0\"><devinfo%20camcnt=\"\"/></juan>";
+                WebClient w = new WebClient();
+                byte[] dataByte = w.DownloadData(ip + data);
+                string str = System.Text.Encoding.Default.GetString(dataByte);
+                string[] parts = str.Split('\n');
+                string parta = parts[1].Substring(20);
+                parta = parta.Replace("></devinfo>", "");
+                parta = parta.Replace("camcnt=", "");
+                parta = parta.Replace("\"", "");
+                int camCount = Int32.Parse(parta);
+                Console.WriteLine("Cameras: " + camCount);
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    label1.Text = camCount.ToString();
+                }));
+                doVisualShit(ip, camCount);
+                return camCount;
+            }
+            else
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    listBox1.Items.Insert(0, "Ping failed.");
+                }));
+                return -1;
+            }
+        }
+
+        public void doVisualShit(string ip, int camcnt)
+        {
+            //640 x 360
+
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                listBox1.Items.Insert(0, "Camcnt: " + camcnt);
+            }));
+
+            if (camcnt == -1)
+            {
+                return;
+            }
+
+            WebClient dl = new WebClient();
+            List<Image> snaps = new List<Image> { };
+            List<Image> snapsbuffer = new List<Image> { };
+
+            string defewaylocation1 = "/cgi-bin/snapshot.cgi?chn=";
+            string defewaylocation2 = "&u=admin&p=";
+            for (int i = 0; i < camcnt; i++){
+                try
+                {
+                    byte[] imgbytes = dl.DownloadData(ip + defewaylocation1 + i + defewaylocation2);
+                    var ms = new MemoryStream(imgbytes);
+                    Image cur = Image.FromStream(ms);
+                    snaps.Add(cur);
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        listBox1.Items.Insert(0, "Got snap " + i);
+                    }));
+                }
+                catch
+                {
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        listBox1.Items.Insert(0, "No cam on " + i);
+                    }));
+                }
+            }
+
+            int len = 640;
+            int hei = 360;
+
+            foreach(Image i in snaps)
+            {
+                snapsbuffer.Add(new Bitmap(i, new Size(len, hei)));
+            }
+
+            snaps = snapsbuffer;
+
+            Bitmap canvas = null;
+
+            List<Point> allSnaPositions = null;
+
+            switch (camcnt)
+            {
+                case 4:
+                    canvas = new Bitmap(len * 2, hei * 2);
+                    allSnaPositions = getAllPositions(2, len, hei);
+                    break;
+                case 8:
+                    canvas = new Bitmap(len * 3, hei * 3);
+                    allSnaPositions = getAllPositions(3, len, hei);
+                    break;
+                case 16:
+                    canvas = new Bitmap(len * 4, hei * 4);
+                    allSnaPositions = getAllPositions(4, len, hei);
+                    break;
+                case 24:
+                    canvas = new Bitmap(len * 5, hei * 5);
+                    allSnaPositions = getAllPositions(5, len, hei);
+                    break;
+            }
+
+            Graphics gr = Graphics.FromImage(canvas);
+            gr.FillRectangle(Brushes.Gray, 0, 0, canvas.Width, canvas.Height);
+            int count = 0;
+            foreach(Bitmap img in snaps)
+            {
+                gr.DrawImage(img, allSnaPositions[count]);
+                count++;
+            }
+
+            Font lucFont = new Font("Lucida Console", 10);
+            PointF Pointvar = new PointF(10f, 10f);
+            SizeF size = gr.MeasureString(ip, lucFont);
+            RectangleF rect = new RectangleF(Pointvar, size);
+            gr.FillRectangle(Brushes.Black, rect);
+            gr.DrawString(ip, lucFont, Brushes.White, Pointvar);
+            string filename = RandomString(5);
+            canvas.Save(selectedDirectory + "/" + filename + ".png");
+            pictureBox1.Image = canvas;
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                listBox1.Items.Insert(0, "Saved.");
+            }));
+
+
+        }
+
+        public static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public List<Point> getAllPositions(int num, int _width, int _height)
+        {
+            List<Point> points = new List<Point> { };
+
+            for (int o = 0; o < num; o++)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    points.Add(new Point(_width * i, _height * o));
+                }
+            }
+            return points;
+        }
+
+        public static int GetNextInt32(RNGCryptoServiceProvider rnd)
+        {
+            byte[] randomInt = new byte[4];
+            rnd.GetBytes(randomInt);
+            return Convert.ToInt32(randomInt[0]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                RNGCryptoServiceProvider rnd2 = new RNGCryptoServiceProvider(); //may be overdoing it a little
+                ips = ips.OrderBy(x => GetNextInt32(rnd2)).ToList();
+            }
+
+            Thread a = new Thread(() => threadManager());
+            a.IsBackground = true;
+            a.Start();
+        }
+
+        public void threadManager()
+        {
+            foreach (string ip in ips)
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    listBox1.Items.Insert(0, "------------");
+                }));
+                try
+                {
+                    getCamCount(ip);
+                }
+                catch
+                {
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        listBox1.Items.Insert(0, "Error in " + ip);
+                    }));
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Input File";
+                dlg.Filter = "Text Files | *.txt";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    ips = System.IO.File.ReadAllLines(dlg.FileName).ToList();
+                    button2.ForeColor = Color.Green;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    selectedDirectory = fbd.SelectedPath + "/";
+                    button3.ForeColor = Color.Green;
+                }
+            }
         }
     }
 }
